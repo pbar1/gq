c22b0feed029a7ee944470d43529212d75522243
Pierce Bartine <pbartine@salesforce.com>
2020-09-10 19:30:19 -0700

    Add usage header to help flag

--- main.go
+++ main.go
@@ -24,6 +24,22 @@ var (
 	outTemplate  = flag.StringP("template", "t", `{{.}}`, "Go template string")
 )
 
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, `Reads from stdin and writes to stdout. Can convert between input and output formats, including Go templates.
+
+Examples:
+  Feed Kubernetes YAML into gq and render it as a Go template
+  $ kubectl get namespaces -o yaml | gq -i yaml -t '{{range .items}}{{.metadata.name}}{{printf "\n"}}{{end}}'
+
+  Convert Terraform HCL into JSON (and feed that into jq for querying!)
+  cat *.tf | gq -i hcl -o json | jq
+`)
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+}
+
 func main() {
 	flag.Parse()
 
