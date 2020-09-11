549f444b68b544a14edf4bd60980bc3cbfa0d40d
Pierce Bartine <pbartine@salesforce.com>
2020-09-10 18:28:25 -0700

    add convert

--- main.go
+++ main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-	"html/template"
 	"io/ioutil"
 	"os"
+	"strings"
+	"text/template"
 
 	"github.com/Masterminds/sprig"
+	"github.com/hashicorp/hcl"
+	"github.com/pelletier/go-toml"
 	flag "github.com/spf13/pflag"
+	"gopkg.in/yaml.v2"
 )
 
 var (
-	version = "unknown"
-	// _            = flag.Bool("help", false, "Help for gq")
+	version      = "unknown"
 	printVersion = flag.Bool("version", false, "Print program version")
+	inputFormat  = flag.StringP("input", "i", "json", "Input format. One of: json|yaml|toml|hcl")
+	outputFormat = flag.StringP("output", "o", "go-template", "Output format. One of: go-template|json|yaml|toml")
 	outTemplate  = flag.StringP("template", "t", `{{.}}`, "Go template string")
 )
 
@@ -38,14 +44,54 @@ func main() {
 	}
 	check(err, "unable to read input from "+filename)
 
+	intermediate, err := input(in, *inputFormat)
+	check(err, "unable to input "+*inputFormat+" input from "+filename)
+
+	out, err := output(intermediate, *outputFormat)
+	check(err, "unable to render output")
+
+	fmt.Println(string(out))
+}
+
+func output(v interface{}, format string) ([]byte, error) {
+	var marshalFn func(v interface{}) ([]byte, error)
+	switch strings.ToLower(*outputFormat) {
+	case "go-template":
+		tpl := template.Must(template.New("go-template").Funcs(sprig.TxtFuncMap()).Parse(*outTemplate))
+		var buf bytes.Buffer
+		if err := tpl.Execute(&buf, v); err != nil {
+			return nil, err
+		}
+		return buf.Bytes(), nil
+	case "json":
+		marshalFn = json.Marshal
+	case "yaml":
+		marshalFn = yaml.Marshal
+	case "toml":
+		marshalFn = toml.Marshal
+	default:
+		return nil, fmt.Errorf("unsupported input format: %s", format)
+	}
+	return marshalFn(v)
+}
+
+func input(in []byte, format string) (interface{}, error) {
+	var unmarshalFn func(data []byte, v interface{}) error
+	switch strings.ToLower(format) {
+	case "json":
+		unmarshalFn = json.Unmarshal
+	case "yaml":
+		unmarshalFn = yaml.Unmarshal
+	case "toml":
+		unmarshalFn = toml.Unmarshal
+	case "hcl":
+		unmarshalFn = hcl.Unmarshal
+	default:
+		return nil, fmt.Errorf("unsupported input format: %s", format)
+	}
 	var v interface{}
-	err = json.Unmarshal(in, &v)
-	check(err, "unable to unmarshal json input from "+filename)
-
-	tpl := template.Must(template.New("base").Funcs(sprig.FuncMap()).Parse(*outTemplate))
-
-	err = tpl.Execute(os.Stdout, v)
-	check(err, "unable to render template")
+	err := unmarshalFn(in, &v)
+	return v, err
 }
 
 func check(err error, msg string) {
